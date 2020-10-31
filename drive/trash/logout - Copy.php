0080747d1deac3ec4bc8f557153952d703377988
kumarp149 <68728539+kumarp149@users.noreply.github.com>
2020-10-31 23:41:40 +0530

    Deleted unwanted images

--- drive/trash/logout - Copy.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['emailid']);
-if (! isset($_SESSION['emailid'])){
-    echo json_encode(array('success' => 1));
-}
-?>
--- drive/trash/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$folder = $_POST['dir'];
-$name = $_POST['content'];
-unlink($name);
-echo json_encode(array('success' => 1));
-
-?>
