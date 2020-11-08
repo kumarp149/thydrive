f07360a31ad5e97f74e2d7500faf4e17c54a4d25
kumarp149 <68728539+kumarp149@users.noreply.github.com>
2020-11-08 15:11:32 +0530

    Modified create-account file

--- create-account.php
+++ create-account.php
@@ -135,15 +135,15 @@ if (isset($_POST['submit'])){
     $mail = new PHPMailer(true);
     $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.mailgun.org';                    // Set the SMTP server to send through
+    $mail->Host       = '******************';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'noreply@mail.sruteesh.icu';                     // SMTP username
-    $mail->Password   = '978868c7cef9e503293aa46c184d9718-0f472795-e069b35a';                               // SMTP password
+    $mail->Username   = '*****************';                     // SMTP username
+    $mail->Password   = '**************************';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('noreply@mail.sruteesh.icu', 'Sruteesh');
+    $mail->setFrom('*****************', 'Sruteesh');
     $mail->addAddress($email, $fname);     // Add a recipient
   /*  $mail->addAddress('ellen@example.com');               // Name is optional
     $mail->addReplyTo('info@example.com', 'Information');
